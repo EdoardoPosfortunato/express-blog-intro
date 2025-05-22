33c82f7d475846eecf1c57612383d64996d214d1
EdoardoPosfortunato <edoardo.posfortunato27@gmail.com>
2025-05-22 16:14:03 +0200

    feat: app.get e app.listen

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // console.log("ciao mondo")
+
+import express from "express";
+
+// console.log(express);
+
+const app = express(); // creazione dell'applicazione vera e propria
+
+// console.log(app);
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('ciao mondo');
+}) // questa parte è quello che rstituisce quando si interroga
+
+app.listen(port, () => {
+    console.log("sto ascoltando")
+}) // questa parte serve per fare in mdoo che il nostro PC che fa da server si metta in aspcolto
